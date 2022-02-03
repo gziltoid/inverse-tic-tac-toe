f28ec3b899512d0ae51dfb670e1a729a3caeb81d
gziltoid <g.ziltoid@gmail.com>
2022-02-04 00:23:09 +0100

    Implemented a basic game model

--- game.py
+++ game.py
@@ -1,23 +1,49 @@
 #!/usr/bin/env python3
+from collections import namedtuple
+from enum import Enum
+
 
 PLAYER_MARKS = ['X', 'O']
 WIDTH, HEIGHT = 10, 10
 
+Coords = namedtuple('Coords', 'x y')
 
-class InverseTicTacToe:
+
+class GameState(Enum):
+    IN_PROGRESS = 1
+    X_WON = 2
+    O_WON = 3
+    TIE = 4
+
+
+class InverseTicTacToeBoard:
 
     def __init__(self):
-        self.board = [[None for _ in range(WIDTH)] for _ in range(HEIGHT)]
+        self.board = [[None] * WIDTH for _ in range(HEIGHT)]
+        self.game_state = GameState.IN_PROGRESS
 
-    def place_marker(self, marker, position):
-        pass
+    def _is_cell_empty(self, cell):
+        return self.board[cell.x][cell.y] is None
 
-    def check_if_player_will_lose(self, marker, position):
+    def _are_there_empty_cells(self):
+        return any(None in row for row in self.board)
+
+    def try_place_marker(self, marker, pos):
+        if not self._is_cell_empty(pos):
+            return False
+        self.board[pos.x][pos.y] = marker
+        if self.check_if_player_will_lose(marker, pos):
+            self.game_state = GameState.X_WON if marker == 'O' else GameState.O_WON
+        elif not self._are_there_empty_cells():
+            self.game_state = GameState.TIE
+        return True
+
+    def check_if_player_will_lose(self, marker, cell):
         pass
 
     def get_result(self):
-        pass
+        return self.game_state
 
 
 if __name__ == '__main__':
-    game = InverseTicTacToe()
+    game = InverseTicTacToeBoard()
