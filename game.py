410875d9d68ba840f3620a83bbc2eb300e94388f
gziltoid <g.ziltoid@gmail.com>
2022-02-04 02:14:13 +0100

    Implemented check_if_player_will_lose() (untested)

--- game.py
+++ game.py
@@ -5,6 +5,7 @@ from enum import Enum
 
 PLAYER_MARKS = ['X', 'O']
 WIDTH, HEIGHT = 10, 10
+LOOSING_LENGTH = 5
 
 Coords = namedtuple('Coords', 'x y')
 
@@ -22,24 +23,92 @@ class InverseTicTacToeBoard:
         self.board = [[None] * WIDTH for _ in range(HEIGHT)]
         self.game_state = GameState.IN_PROGRESS
 
-    def _is_cell_empty(self, cell):
+    def __is_cell_empty(self, cell):
         return self.board[cell.x][cell.y] is None
 
-    def _are_there_empty_cells(self):
+    def __are_there_empty_cells(self):
         return any(None in row for row in self.board)
 
     def try_place_marker(self, marker, pos):
-        if not self._is_cell_empty(pos):
+        if not self.__is_cell_empty(pos):
             return False
         self.board[pos.x][pos.y] = marker
         if self.check_if_player_will_lose(marker, pos):
             self.game_state = GameState.X_WON if marker == 'O' else GameState.O_WON
-        elif not self._are_there_empty_cells():
+        elif not self.__are_there_empty_cells():
             self.game_state = GameState.TIE
         return True
 
-    def check_if_player_will_lose(self, marker, cell):
-        pass
+    def check_if_player_will_lose(self, marker, pos):
+        count = 1
+        # check left
+        for cell in self.board[pos.x][pos.y - 1::-1]:
+            if cell == marker:
+                count += 1
+            else:
+                break
+        # check right
+        for cell in self.board[pos.x][pos.y + 1:]:
+            if cell == marker:
+                count += 1
+            else:
+                break
+
+        if count == LOOSING_LENGTH:
+            return True
+        else:
+            count = 1
+
+        # check above
+        for cell in self.board[pos.x - 1::-1][pos.y]:
+            if cell == marker:
+                count += 1
+            else:
+                break
+        # check below
+        for cell in self.board[pos.x + 1:][pos.y]:
+            if cell == marker:
+                count += 1
+            else:
+                break
+
+        if count == LOOSING_LENGTH:
+            return True
+        else:
+            count = 1
+
+        # check left diagonal upward
+        for cell in self.board[pos.x - 1::-1][pos.y - 1::-1]:
+            if cell == marker:
+                count += 1
+            else:
+                break
+        # check left diagonal downward
+        for cell in self.board[pos.x + 1:][pos.y + 1:]:
+            if cell == marker:
+                count += 1
+            else:
+                break
+
+        # check right diagonal upward
+        for cell in self.board[pos.x - 1::-1][pos.y + 1:]:
+            if cell == marker:
+                count += 1
+            else:
+                break
+        # check right diagonal downward
+        for cell in self.board[pos.x + 1:][pos.y - 1::-1]:
+            if cell == marker:
+                count += 1
+            else:
+                break
+
+        if count == LOOSING_LENGTH:
+            return True
+        else:
+            count = 1
+
+        return False
 
     def get_result(self):
         return self.game_state
